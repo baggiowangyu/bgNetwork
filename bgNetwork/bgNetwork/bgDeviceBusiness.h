b8333f6385ca3931adae8893e9675fbd678b113b
baggiowangyu <baggiowangyu@gmail.com>
2018-12-18 23:29:32 +0800

    一点点改动

--- bgNetwork/bgNetwork/bgDeviceBusiness.h
+++ bgNetwork/bgNetwork/bgDeviceBusiness.h
@@ -1,10 +1,31 @@
 #ifndef _bgDeviceBusiness_H_
 #define _bgDeviceBusiness_H_
 
+#define MAKE_MAGIC_CODE(a,b,c,d)	(a | (b<<8) | (c<<16) | (d<<24))
+#define NETWORK_MAGIC_CODE			MAKE_MAGIC_CODE('G', 'X', 'X', 'H')
+
+typedef enum
+{
+	CMDID_RequstHeartBeat		= 10001001,
+	CMDID_ResponseHeartBeat		= 10002001,
+
+	CMDID_RequestRealplay		= 20001001,
+	CMDID_RequestTalkback		= 20001027,
+
+	CMDID_PostVioceUpData		= 3008,
+
+	CMDID_PostClientDeviceInfo	= 3022,
+	CMDID_PostDeviceInfo	= 3220,
+	CMDID_PostGpsInfo		= 3221,
+	CMDID_PostSosInfo		= 3222,
+	CMDID_PostDeviceState	= 3223
+
+} enNetCmd;
+
 #pragma pack(1)
 typedef struct _GxxGmDevMsgHead_V1_
 {
-	unsigned char magic_[4];
+	unsigned int magic_;
 	unsigned int packet_size_;
 	unsigned int command_id_;
 
